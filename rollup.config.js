88f449b143a34b65448ad891ac3f3ee6001203d3
vandeurenglenn <vandeurenglenn@gmail.com>
2022-06-01 22:08:24 +0200

    Upgrade chain and build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import json from '@rollup/plugin-json'
 
 export default [{
-  input: 'src/www/shell.js',
+  input: ['src/www/shell.js', 'src/www/views/wallet.js', 'src/www/views/stats.js', 'src/www/views/validator.js'],
   output: [{
     format: 'es',
     dir: 'app'
-  }]
+  }],
+  plugins: [
+    json()
+  ]
 }, {
   input: 'src/www/chain.js',
   output: [{
