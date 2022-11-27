66648f695900524f029036e94a5b0552c9ad6493
vandeurenglenn <vandeurenglenn@gmail.com>
2022-11-27 20:52:38 +0100

    api.pubsub & deploy

--- src/preload.js
+++ src/preload.js
@@ -11,7 +11,7 @@ import { readFile } from 'fs/promises';
   contextBridge.exposeInMainWorld('api', {
     peerId: () => api.peerId,
     accounts: api.accounts,
-    nativeToken: () => api.nativeToken,
+    nativeToken: api.nativeToken,
     peers: () => api.peers,
     validators: () => api.validators,
     peernet: () => api.peernet,
@@ -21,7 +21,13 @@ import { readFile } from 'fs/promises';
     createTransactionFrom: params => api.createTransactionFrom(params),
     readFile: (path) => readFile(path),
     selectedAccount: () => api.selectedAccount(),
-    selectAccount: address => api.selectAccount(address)
+    selectAccount: address => api.selectAccount(address),
+    deploy: api.deploy,
+    pubsub: {
+      subscribers: pubsub.subscribers,
+      subscribe: (ev, fn) => pubsub.subscribe(ev, fn),
+      publish: (ev, data) => pubsub.publish(ev, data)
+    }
   })
   const shell = document.querySelector('app-shell')
   shell.setAttribute('api-ready', '')
